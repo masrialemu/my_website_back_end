4678d29d4235e319ff4b048cb26148639691d07e
Masri Alemu <masrialemu@gmail.com>
2023-11-01 16:11:05 +0300

    v1.11

--- Database/Content.js
+++ Database/Content.js
@@ -1,12 +1,12 @@
 const mongo = require('mongoose')
 const Port = new mongo.Schema({
     public_id:{type:Number,default: 1 ,unique: true},
-    c1:{type:String,unique: true},
-    c2:{type:String ,unique: true},
-    c3:{type:String ,unique: true},
-    c4:{type:String ,unique: true},
-    cv:{type:String ,unique: true},
-    desc:{type:String,unique: true},
+    c1:{type:String,unique: true, required: true},
+    c2:{type:String ,unique: true, required: true},
+    c3:{type:String ,unique: true, required: true},
+    c4:{type:String ,unique: true, required: true},
+    cv:{type:String ,unique: true, required: true},
+    desc:{type:String,unique: true, required: true},
     image:{type:String}
 },{timestamps:true})
 
