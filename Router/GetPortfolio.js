a1b7ffca9f4d3f8aa0928264b5c615dcd63d9715
Masri Alemu <masrialemu@gmail.com>
2023-11-01 14:38:58 +0300

    v1.9

--- Router/GetPortfolio.js
+++ Router/GetPortfolio.js
@@ -2,11 +2,15 @@ const express = require('express');
 const App = express.Router();
 const data = require('./../Database/Port');
 
-// GET route to fetch all data
+// GET route to fetch all data in reverse order
 App.get('/project', async (req, res) => {
   try {
     const allData = await data.find();
-    res.status(200).json(allData);
+
+    // Reverse the order of the data array
+    const reversedData = allData.reverse();
+
+    res.status(200).json(reversedData);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Server error.' });
